018c865d99fd738329b48f5e67a8793d2a8b5a47
Emil Eriksson <asheidan@gmail.com>
2020-08-12 17:35:37 +0200

    Minimum working graph

--- collect_graphs.py
+++ collect_graphs.py
@@ -30,73 +30,34 @@ unwanted_patterns = (
 )
 
 
-class Graph:
-    def __init__(self, name: str):
-        self.name = name
-
-        self.nodes = []
-        self.links = []
-
-
-    @classmethod
-    def from_file(cls, filepath: str):
-        basename = os.path.basename(filepath)
-        root, extension = os.path.splitext(basename)
-
-        with open(filepath, "r") as graph_file:
-            filecontent = graph_file.readlines()
-
-        graph = cls(name=root)
-
-        for line in filecontent[4:-2]:
-            line = line.strip()
-            if not wanted_line(line):
-                continue
-
-            #if is_edge(line):
-            #    print("E: %s" % line)
-
-            if is_node(line):
-                graph.nodes.append(node_from_line(line, graph=graph))
-
-        return graph
-
-        #map(split_label, map(remove_aws, filter(wanted_line, filecontent[4:-2]))),
-
-    def __str__(self):
-        return "\n".join([
-            f'subgraph "cluster_{self.name}" {{',
-            f'\tlabel = "{self.name}";',
-            '',
-            '\t' + '\n\t'.join(map(str, self.nodes)),
-            '}',
-        ])
-
-
 class Node:
 
     node_line = re.compile(r'"\[root\] (?P<name>[^"]*)" \[(?P<attributes>[^]]*)\]')
     attribute_sep = re.compile(r' *, *')
     key_value_sep = re.compile(r' *= *')
 
-    def __init__(self, name: str, graph: Optional[Graph] = None,
-                 attributes: Optional[Dict] = None):
+    def __init__(self, name: str, attributes: str, graph):
         self.name = name
         self.graph = graph
 
-        if attributes:
-            self.label = attributes.pop('label')
+        attribute_map = self._parse_attributes(attributes)
 
-        self.attributes = attributes
+        self.label = attribute_map.pop('label')
 
-    @classmethod
-    def valid_line(cls, line: str) -> bool:
-        return False
+        self.attributes = attribute_map
 
     @classmethod
-    def attributes(cls, line: str):
-        return dict(cls.key_value_sep.split(pair)
-                    for pair in cls.attribute_sep.split(line))
+    def valid_line(cls, line: str) -> Optional[re.Match]:
+        # print(cls.__name__)
+        return cls.node_line.fullmatch(line)
+
+    @classmethod
+    def from_match(cls, match, graph):
+        return cls(**match.groupdict(), graph=graph)
+
+    def _parse_attributes(self, line: str):
+        return dict(self.key_value_sep.split(pair)
+                    for pair in self.attribute_sep.split(line))
 
     def attributes_as_str(self) -> str:
         return ','.join('='.join(p) for p in self.attributes.items())
@@ -110,13 +71,24 @@ class Resource(Node):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.resource, self.key = self.label.split('.')
+        label_parts = self.label[1:-1].split('.')
+        self.resource, self.key = label_parts[-2:]
+
+        if self.resource.startswith('aws_'):
+            self.resource = self.resource[4:]
+
+        self.is_data = len(label_parts) == 3 and label_parts[0] == 'data'
+
+        if len(label_parts) == 4:
+            self.module_name = label_parts[1]
 
     @property
     def label_formatted(self):
-        return ('label=<'
+        type_indicator = '<font color="gray60" point-size="9">data.</font>' if self.is_data else ''
+        return ('<'
                 '<table border="0">'
                 '<tr><td>'
+                f'{type_indicator}'
                 f'<font color="gray40" point-size="9">{self.resource}</font>'
                 '</td></tr>'
                 f'<tr><td>{self.key}</td></tr>'
@@ -125,67 +97,127 @@ class Resource(Node):
     def __str__(self):
         return f'"[{self.graph.name}] {self.name}" [label={self.label_formatted},{self.attributes_as_str()}]'
 
-    @classmethod
-    def valid_line(cls, line: str) -> bool:
-        return 'label = "aws_' in line
-
 
 class Variable(Resource):
-    @classmethod
-    def valid_line(cls, line: str) -> bool:
-        return 'label = "var.' in line
+    node_line = re.compile(r'"\[root\] (?P<name>var\.[^"]*)" \[(?P<attributes>[^]]*)\]')
 
 
 class Provider(Resource):
     @classmethod
-    def valid_line(cls, line: str) -> bool:
+    def valid_line(cls, line: str) -> Optional[re.Match]:
         return 'label = "provider.' in line
-
-
-class Data(Node):
-    @classmethod
-    def valid_line(cls, line: str) -> bool:
-        return 'label = "data.' in line
-
-
-class Module(Node):
-    @classmethod
-    def valid_line(cls, line: str) -> bool:
-        return 'label = "module.' in line
 
 
 class Output(Node):
     @classmethod
-    def valid_line(cls, line: str) -> bool:
+    def valid_line(cls, line: str) -> Optional[re.Match]:
         return 'label = "output.' in line
 
 
-NODE_CLASSES = [
-    Resource,
-    Variable,
-    Provider,
-    Data,
-    Module,
-    Output,
-]
+class Edge:
+    edge_line = re.compile(r'"\[root\] (?P<from_name>[^"]*)" -> "\[root\] (?P<to_name>[^"]*)"')
+
+    def __init__(self, from_name, to_name, graph):
+        self.from_name = from_name
+        self.to_name = to_name
+
+        self.graph = graph
+
+    def __str__(self):
+        return f'"[{self.graph.name}] {self.from_name}":e -> "[{self.graph.name}] {self.to_name}":w'
+
+    @classmethod
+    def valid_line(cls, line: str) -> Optional[re.Match]:
+        # print(cls.__name__)
+        return cls.edge_line.fullmatch(line)
+
+    @classmethod
+    def from_match(cls, match, graph):
+        return cls(**match.groupdict(), graph=graph)
 
 
-def node_from_line(line: str, graph: Graph):
-    for cls in NODE_CLASSES:
-        if cls.valid_line(line):
-            break
-    else:
-        raise ValueError()
+class Graph:
 
-    match = cls.node_line.fullmatch(line)
+    module_line = re.compile(r'"\[root\] module\.(?P<module_name>[^.]*)\.(?P<name>[^"]*)" \[(?P<attributes>[^]]*)\]')
 
-    node = cls(name=match['name'], graph=graph,
-               attributes=cls.attributes(match['attributes']))
+    NODE_CLASSES = [
+        Resource,
+        Variable,
+        Provider,
+        Output,
+    ]
+    def __init__(self, name: str):
+        self.name = name
 
-    return node
+        self.nodes = []
+        self.edges = []
+        self.modules = {}
 
 
+    @classmethod
+    def from_file(cls, filepath: str):
+        basename = os.path.basename(filepath)
+        root, _extension = os.path.splitext(basename)
 
+        with open(filepath, "r") as graph_file:
+            filecontent = graph_file.readlines()
+
+        graph = cls(name=root)
+
+        for line in filecontent[4:-2]:
+            line = line.strip()
+            # print(line)
+            if not wanted_line(line):
+                continue
+
+            match = Edge.valid_line(line)
+            if match:
+                edge = Edge.from_match(match, graph=graph)
+                graph.edges.append(edge)
+
+                continue
+
+            node = None
+            module = None
+
+            match = cls.module_line.fullmatch(line)
+            if match:
+                module_name = match['module_name']
+                if module_name in graph.modules:
+                    module = graph.modules[module_name]
+                else:
+                    module = Graph(name=graph.name + "-" + module_name)
+                    graph.modules[module_name] = module
+
+            for node_class in [Variable, Resource]:
+                match = node_class.valid_line(line)
+                if match:
+                    node = node_class.from_match(match, graph=graph)
+                    break
+
+
+            if node:
+                if module:
+                    module.nodes.append(node)
+                else:
+                    graph.nodes.append(node)
+
+        return graph
+
+        #map(split_label, map(remove_aws, filter(wanted_line, filecontent[4:-2]))),
+
+    def __str__(self):
+        return "\n".join([
+            f'subgraph "cluster_{self.name}" {{',
+            f'\tlabel = "{self.name}";',
+            '',
+            '\t' + '\n\t'.join(map(str, self.modules.values())),
+            '',
+            '\t' + '\n\t'.join(map(str, self.nodes)),
+            '',
+            '\t' + '\n\t'.join(map(str, self.edges)),
+            '}',
+        ])
 
 
 def wanted_line(line: str) -> bool:
@@ -222,8 +254,6 @@ def remove_aws(line: str) -> str:
 
 def main() -> None:
     subgraphs = [Graph.from_file(filepath) for filepath in sys.argv[1:]]
-    print("\n".join(map(str, subgraphs)))
-    return
 
     print('digraph root {\n'
           '\tcompound = "true";\n'
@@ -233,7 +263,7 @@ def main() -> None:
           '\tedge[arrowsize = 0.6];\n'
           '\tnode[fontname = "helvetica", fontsize = 10]',
           )
-    print(subgraphs)
+    print("\n".join(map(str, subgraphs)))
     print(
         #'"[bootstrap] aws_s3_bucket.terraform_state_storage" -> "[xandr-integration] root"'
         '\t"[xandr-integration] provider.terraform (close)"[label="provider.terraform"];\n'
